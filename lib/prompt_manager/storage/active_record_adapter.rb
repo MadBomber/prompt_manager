a89ee6612f4d94f4ddcbb44accde6ffde7d8de37
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-11-28 23:21:59 -0600

    could not release without these files being committed

--- lib/prompt_manager/storage/active_record_adapter.rb
+++ lib/prompt_manager/storage/active_record_adapter.rb
@@ -64,3 +64,98 @@ class PromptManager::Storage::ActiveRecordAdapter
     model_class.find_by(id: prompt_id) || raise('Prompt not found')
   end
 end
+
+
+__END__
+
+# prompt_manager/lib/prompt_manager/storage/active_record_adapter.rb
+
+require 'active_record'
+
+module PromptManager
+  module Storage
+    class ActiveRecordAdapter
+
+      # Define models for ActiveRecord
+      class Prompt < ActiveRecord::Base
+        validates :unique_id, presence: true
+        validates :text, presence: true
+      end
+
+      class PromptParameter < ActiveRecord::Base
+        belongs_to :prompt
+        validates :key, presence: true
+        serialize :value
+      end
+
+      def initialize
+        unless ActiveRecord::Base.connected?
+          raise ArgumentError, "ActiveRecord is not connected"
+        end
+      end
+
+      def get(id:)
+        prompt = Prompt.find_by(unique_id: id)
+        return nil unless prompt
+
+        parameters = prompt.prompt_parameters.index_by(&:key)
+
+        {
+          id:         prompt.unique_id,
+          text:       prompt.text,
+          parameters: parameters.transform_values(&:value)
+        }
+      end
+
+      def save(id:, text: "", parameters: {})
+        prompt = Prompt.find_or_initialize_by(unique_id: id)
+        prompt.text = text
+        prompt.save!
+
+        parameters.each do |key, value|
+          parameter = PromptParameter.find_or_initialize_by(prompt: prompt, key: key)
+          parameter.value = value
+          parameter.save!
+        end
+      end
+
+      def delete(id:)
+        prompt = Prompt.find_by(unique_id: id)
+        return unless prompt
+        
+        prompt.prompt_parameters.destroy_all
+        prompt.destroy
+      end
+
+      def search(for_what)
+        query = '%' + for_what.downcase + '%'
+        Prompt.where('LOWER(text) LIKE ?', query).pluck(:unique_id)
+      end
+
+      def list(*)
+        Prompt.pluck(:unique_id)
+      end
+
+      private
+
+      # This is an example of how the database connection setup could look like, 
+      # but it should be handled externally in the actual application setup.
+      def self.setup_database_connection
+        ActiveRecord::Base.establish_connection(
+          adapter: 'sqlite3',
+          database: 'prompts.db'
+        )
+      end
+    end
+  end
+end
+
+# After this, you would need to create a database migration to generate the required tables.
+# Additionally, you have to establish an ActiveRecord connection before using this adapter,
+# typically in the environment setup of your application.
+
+# Keep in mind you need to create migrations for both the Prompt and PromptParameter models,
+# and manage the database schema using ActiveRecord migrations. This adapter assumes that the
+# database structure is already in place and follows the schema inferred by the models in the adapter.
+
+
--- lib/prompt_manager/storage/sqlite_adapter.rb
+++ lib/prompt_manager/storage/sqlite_adapter.rb
@@ -61,5 +61,96 @@ class PromptManager::Storage::SqliteAdapter
 end
 
 
+__END__
+
+
+require 'sequel'
+require 'pathname'
+require 'json'
+
+module PromptManager
+  module Storage
+    class SqliteAdapter
+      DEFAULT_DB_FILE = 'prompts.sqlite3'.freeze
+
+      class << self
+        attr_accessor :db_file, :db
+
+        def config
+          if block_given?
+            yield self
+          else
+            raise ArgumentError, 'No block given to config'
+          end
+
+          self.db = Sequel.sqlite(db_file || DEFAULT_DB_FILE) # Use provided db_file or default
+          create_tables unless db.table_exists?(:prompts)
+        end
+
+        # Define the necessary tables within the SQLite database if they don't exist
+        def create_tables
+          db.create_table :prompts do
+            primary_key :id
+            String :prompt_id, unique: true, null: false
+            Text :text
+            Json :parameters
+
+            index :prompt_id
+          end
+        end
+      end
+
+      def initialize
+        @db = self.class.db
+      end
+
+      def get(id:)
+        validate_id(id)
+        result = @db[:prompts].where(prompt_id: id).first 
+        raise ArgumentError, 'Prompt not found' unless result
+
+        {
+          id:         result[:prompt_id],
+          text:       result[:text],
+          parameters: result[:parameters]
+        }
+      end
+
+      def save(id:, text: '', parameters: {})
+        validate_id(id)
+        rec = @db[:prompts].where(prompt_id: id).first
+        if rec
+          @db[:prompts].where(prompt_id: id).update(text: text, parameters: Sequel.pg_json(parameters))
+        else
+          @db[:prompts].insert(prompt_id: id, text: text, parameters: Sequel.pg_json(parameters))
+        end
+      end
+
+      def delete(id:)
+        validate_id(id)
+        @db[:prompts].where(prompt_id: id).delete
+      end
+
+      # Return an Array of prompt IDs
+      def list()
+        @db[:prompts].select_map(:prompt_id)
+      end
+
+      def search(for_what)
+        search_term = for_what.downcase
+        @db[:prompts].where(Sequel.ilike(:text, "%#{search_term}%")).select_map(:prompt_id)
+      end
+
+      private
+
+      # Validate that the ID contains good characters.
+      def validate_id(id)
+        raise ArgumentError, "Invalid ID format id: #{id}" unless id =~ /^[a-zA-Z0-9\-\/_]+$/
+      end
+    end
+  end
+end
+
+
 
 
